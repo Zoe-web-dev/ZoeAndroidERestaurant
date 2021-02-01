4311d2dc73f3dd0dcee028a10ba224f8793c4629
Zoe <Zoe-web-dev@users.noreply.github.com>
2021-02-01 18:50:24 +0100

    Mise en place de la Base Activity pour la toolbar à finir Panier à jour il me manque la pastille via la toolbar

--- app/src/main/java/fr/isen/zoe/androiderestaurant/BasketDetailsActivity.kt
+++ app/src/main/java/fr/isen/zoe/androiderestaurant/BasketDetailsActivity.kt
@@ -1,22 +1,17 @@
 package fr.isen.zoe.androiderestaurant
 
 import APIservices.JsonBasket
-import APIservices.JsonItemBasket
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
 import androidx.core.view.isVisible
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.google.gson.Gson
 import com.google.gson.GsonBuilder
 import fr.isen.zoe.androiderestaurant.databinding.ActivityBasketDetailsBinding
-import fr.isen.zoe.androiderestaurant.databinding.ActivityDetailsCategoryBinding
 import java.io.File
-import java.io.FileNotFoundException
 
 private lateinit var binding: ActivityBasketDetailsBinding
 
-class BasketDetailsActivity : AppCompatActivity() {
+class BasketDetailsActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,15 +20,42 @@ class BasketDetailsActivity : AppCompatActivity() {
         readFile()
     }
 
-    private fun readFile(){
+    private fun readFile() {
         val gson = GsonBuilder().setPrettyPrinting().create()
         val file = File(cacheDir.absolutePath + "/${DetailsCategoryActivity.BASKET_FILE}")
-        if (file.exists()){
+        if (file.exists()) {
             val basket = gson.fromJson(file.readText(), JsonBasket::class.java)
             val foodRecycler = binding.listItemsBasket
-            foodRecycler.adapter = BasketDetailsAdapter(basket.items.toMutableList(),applicationContext)
+            foodRecycler.adapter = BasketDetailsAdapter(basket.items.toMutableList()) {
+                basket.items.remove(it)
+                resetBasket(basket)
+            }
             foodRecycler.layoutManager = LinearLayoutManager(this)
             foodRecycler.isVisible = true
         }
+    }
+
+    //fonction qui met à jour le fichier json (panier)
+    fun resetBasket(basket: JsonBasket) {
+        val file = File(cacheDir.absolutePath + "/$BASKET_FILE")
+        //met a jour le nombre d'items dans le panier
+        saveInMemory(basket, file)
+    }
+
+    private fun saveInMemory(basket: JsonBasket, file: File) {
+        saveDishCount(basket)
+        file.writeText(GsonBuilder().create().toJson(basket))
+    }
+
+    private fun saveDishCount(basket: JsonBasket) {
+        val count = basket.items.sumOf { it.quantity }
+        val sharedPreferences = getSharedPreferences(APP_PREFS, MODE_PRIVATE)
+        sharedPreferences.edit().putInt(BASKET_COUNT, count).apply()
+    }
+
+    companion object {
+        const val APP_PREFS = "app_prefs"
+        const val BASKET_COUNT = "basket_count"
+        const val BASKET_FILE = "basket.json"
     }
 }
--- app/src/main/java/fr/isen/zoe/androiderestaurant/BasketDetailsAdapter.kt
+++ app/src/main/java/fr/isen/zoe/androiderestaurant/BasketDetailsAdapter.kt
@@ -1,10 +1,15 @@
 package fr.isen.zoe.androiderestaurant
 
+import APIservices.APIdish
 import APIservices.JsonItemBasket
 import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.AdapterView
+import androidx.core.app.ActivityCompat.invalidateOptionsMenu
 import androidx.recyclerview.widget.RecyclerView
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
 import com.squareup.picasso.Picasso
 import fr.isen.zoe.androiderestaurant.databinding.ActivityBasketDetailsBinding
 import fr.isen.zoe.androiderestaurant.databinding.BasketItemCellBinding
@@ -12,10 +17,9 @@ import fr.isen.zoe.androiderestaurant.databinding.CategoryCellBinding
 
 private lateinit var binding: BasketItemCellBinding
 
+//deleteItemClickListener est une fonction
 class BasketDetailsAdapter(
-    private val data: MutableList<JsonItemBasket>,
-    applicationContext: Context
-) : RecyclerView.Adapter<BasketDetailsAdapter.BasketHolder>() {
+    private val data: MutableList<JsonItemBasket>, private val deleteItemClickListener: (JsonItemBasket)->Unit) : RecyclerView.Adapter<BasketDetailsAdapter.BasketHolder>() {
     override fun onCreateViewHolder(
         parent: ViewGroup,
         viewType: Int
@@ -28,8 +32,10 @@ class BasketDetailsAdapter(
         val data: JsonItemBasket = data[position]
         //on affiche le titre du dish
         holder.title.text = data.item.title
-        //on affiche le prix du dish
-        holder.totalPrice.text = data.item.getPriceItem()
+        //on affiche le prix total
+        holder.totalPrice.text = (data.quantity * data.item.getFormattedPrice().toDouble()).toString()
+        //on affiche la quantité totale
+        holder.totalQty.text = data.quantity.toString()
         //on affiche une image du plat
         val image = data.item.getImage()
         if (image != null && image.isNotEmpty()) {
@@ -38,17 +44,37 @@ class BasketDetailsAdapter(
                 .placeholder(R.drawable.ic_baseline_image_search_24)
                 .into(holder.image)
         }
+
+        //fonction supprimer un item
+        holder.delete.setOnClickListener {
+            //supprime l'item de l'affichage
+            deleteItem(position)
+            //supprime dans le fichier json
+            deleteItemClickListener.invoke(data)
+        }
     }
 
     override fun getItemCount(): Int {
         return data.size
     }
 
+    fun deleteItem(position: Int) {
+        if(data[position].quantity != 1){
+            data[position].quantity--
+        }else{
+            data.removeAt(position)
+        }
+        notifyDataSetChanged()
+    }
+
+
+
     class BasketHolder(binding: BasketItemCellBinding) : RecyclerView.ViewHolder(binding.root) {
         val title = binding.textItemBasket
+        val totalQty = binding.quantity
         val layout = binding.root
         val totalPrice = binding.totalPrice
         val image = binding.imageViewItemBasket
+        val delete = binding.buttonRemoveItemBasket
     }
 }
-
