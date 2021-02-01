e1605020ffaa59ae03a78e06c93d06fd4932ed8e
Zoe <Zoe-web-dev@users.noreply.github.com>
2021-02-01 12:12:01 +0100

    mise en place de l'activité "Votre panier"

---
+++ app/src/main/java/fr/isen/zoe/androiderestaurant/BasketDetailsActivity.kt
@@ -0,0 +1,11 @@
+package fr.isen.zoe.androiderestaurant
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BasketDetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_basket_details)
+    }
+}
---
+++ app/src/main/java/fr/isen/zoe/androiderestaurant/BasketDetailsAdapter.kt
@@ -0,0 +1,4 @@
+package fr.isen.zoe.androiderestaurant
+
+class BasketDetailsAdapter {
+}
